413bff46bc008700161e4151f19bd7dbd5d4f6e7
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 23:46:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog 
+  def bark 
+    puts "woof!"
+  end
+  
+  def name=(dog_name)
+    @the_dog_name = dog_name
+  end
+  
+  def name 
+    @the_dog_name
+  end
+end
