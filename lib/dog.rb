962950dd7afebcdbcb524e9ff70e2306a09f3aa7
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 23:46:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-class # Your code goes here!
+class Dog 
+  def bark 
+    puts "woof!"
+  end
+  
+  def name=(dog_name)
+    @the_dog_name = dog_name
+  end
+  
+  def name 
+    @@the_dog_name
+end
